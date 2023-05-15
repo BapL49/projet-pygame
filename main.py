e8ca5027b27aa333217c07d154a90a7898d9e095
BapL49 <118617986+BapL49@users.noreply.github.com>
2023-05-15 10:42:47 +0200

    écriture

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ def draw_window(surface, grid, score=0, last_score = 0):
 
     surface.blit(label, (sx + 20, sy + 160))
     # last score
-    label = font.render('High Score: ' + last_score, 1, (255,255,255))
+    label = font.render('Meilleur score: ' + last_score, 1, (255,255,255))
 
     sx = top_left_x - 200
     sy = top_left_y + 200
